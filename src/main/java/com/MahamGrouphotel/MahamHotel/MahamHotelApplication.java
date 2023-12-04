733a96d3d9edab3eb75b79a8bf44355758b3c076
winmoh <mohamedaouinti2023@gmail.com>
2023-12-04 22:11:37 +0100

    spring boot project intializing and dependency addin

---
+++ src/main/java/com/MahamGrouphotel/MahamHotel/MahamHotelApplication.java
@@ -0,0 +1,13 @@
+package com.MahamGrouphotel.MahamHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahamHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahamHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MahamGrouphotel/MahamHotel/MahamHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MahamGrouphotel.MahamHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahamHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
